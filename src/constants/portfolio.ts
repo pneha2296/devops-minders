150f42d1e0655e0e26929167bfc27c123e319220
Neha Prajapati <neha@ganeshasystem.com>
2026-05-21 18:32:29 +0530

    changes

--- src/constants/portfolio.ts
+++ src/constants/portfolio.ts
@@ -7,7 +7,7 @@ export const portfolioProjects: PortfolioProject[] = [
     title: 'Sunset Stretch & Sip',
     description: 'Golden Hour Farm Experience',
     image: '/assets/images/photo1.png',
-    link: '#',
+    link: 'https://www.loom.com/share/fcd3c850c2f6495497fd2ca8bc477f96',
   },
   {
     id: '2',
@@ -15,7 +15,7 @@ export const portfolioProjects: PortfolioProject[] = [
     title: 'Digital Product Ul Design',
     description: '',
     image: '/assets/images/photo2.png',
-    link: '#',
+    link: 'https://www.loom.com/share/727e1df97dae44ac9ad7b8cef99c914d',
   },
   {
     id: '3',
@@ -23,7 +23,7 @@ export const portfolioProjects: PortfolioProject[] = [
     title: 'Match@Health Desktop',
     description: 'Healthcare Matching Platform',
     image: '/assets/images/photo3.png',
-    link: '#',
+    link: 'https://www.loom.com/share/d5786444918f45339e41d50b16dd733a',
   },
   {
     id: '4',
@@ -31,23 +31,23 @@ export const portfolioProjects: PortfolioProject[] = [
     title: 'Match@Health Mobile',
     description: 'Mobile Healthcare Experience',
     image: '/assets/images/photo4.png',
-    link: '#',
+    link: 'https://www.loom.com/share/901d2ac102a240b6ab54477987c57232',
   },
   {
     id: '5',
-    category: 'Digital Agency Platform',
-    title: 'Kubernetes Platform Migration',
+    category: 'UI/UX Design',
+    title: 'Digital Agency Platform',
     description: 'Creative Business Solutions',
     image: '/assets/images/photo5.png',
-    link: '#',
+    link: 'https://www.loom.com/share/da053af75b394a84aa0893bd36a2cf57',
   },
   {
     id: '6',
-    category: 'Logo Design System',
-    title: 'Healthcare Cloud Architecture',
+    category: 'UI/UX Design',
+    title: 'Logo Design System',
     description: 'Modern Brand Identity',
     image: '/assets/images/photo6.png',
-    link: '#',
+    link: 'https://www.loom.com/share/05d44a26667847d1bbb4947bd3716036',
   },
 ]
 
